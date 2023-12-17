06fbe6d746b9f2d3eb636bb76bca3ccc0f9eb3a9
s303 <s303@E10>
2023-12-17 08:48:35 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+class ChristmasTree
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wpisz ile poziomów ma mieć drzewko:");
+        int treeLevels = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wpisz ile poziomów ma mieć pień:");
+        int trunkHeight = int.Parse(Console.ReadLine());
+
+        while (true) 
+        {
+            Console.Clear();
+            GenerateTree(treeLevels);
+            GenerateTrunk(treeLevels, trunkHeight);
+            Thread.Sleep(1000); 
+        }
+    }
+
+    static void GenerateTree(int levels)
+    {
+        for (int i = 0; i < levels; i++)
+        {
+            
+            Console.Write(new string(' ', levels - i - 1));
+
+           
+            for (int j = 0; j < (2 * i + 1); j++)
+            {
+                if (RandomLight() && j % 2 == 0) 
+                {
+                    Console.ForegroundColor = GetRandomConsoleColor();
+                    Console.Write('o');
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write('*');
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void GenerateTrunk(int levels, int height)
+    {
+        Console.ForegroundColor = ConsoleColor.Gry;
+        for (int i = 0; i < height; i++)
+        {
+            Console.Write(new string(' ', levels - 1));
+            Console.WriteLine("|||");
+        }
+        Console.ResetColor();
+    }
+
+    static bool RandomLight()
+    {
+        Random rnd = new Random();
+        return rnd.Next(2) == 0; // Randomly return true or false
+    }
+
+    static ConsoleColor GetRandomConsoleColor()
+    {
+        var consoleColors = Enum.GetValues(typeof(ConsoleColor));
+        Random rnd = new Random();
+        return (ConsoleColor)consoleColors.GetValue(rnd.Next(consoleColors.Length));
+    }
+}
